c285a298c1cba5383bb6ae34c6468d5d32ed6d3a
Gabriel <gabrielgranadam@gmail.com>
2024-11-02 23:06:16 +0100

    Se alteran 2 líneas del documento notificaciones.js

--- notificaciones.js
+++ notificaciones.js
@@ -1,8 +1,8 @@
 //linea de programa 1
 //linea de programa 2
 //linea de programa 3
-//linea de programa 4
+//linea de programa 4 -- Eliminar
 //linea de programa 5
 //linea de programa 6
-//linea de programa 7
+//linea de programa 7 -- Eliminar
 //linea de programa 8
